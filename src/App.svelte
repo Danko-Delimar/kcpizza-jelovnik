fbd675a08b698cca9934dfc74e13b18d33b1f352
Danko-Delimar <danko.delimar@gmail.com>
2020-10-29 17:51:40 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+	let src = "./images/jelovnik.svg"
+	let jelovnik = true
+
+	const toggleButton = (page) => {
+		if (page === "jelovnik") {
+			jelovnik = true
+			src = "./images/jelovnik.svg"
+		} else if (page === "ponude") {
+			jelovnik = false
+			src = "./images/ponude.svg"
+		}
+	}
+</script>
+
+<main>
+	<h1>KC PIZZA JELOVNIK</h1>
+	<p class="text-lg" id="address">Ulica Antuna Nemčića 2, 48000 Koprivnica</p>
+	<div class="buttons">
+		<button on:click={() => toggleButton("jelovnik")} id={jelovnik ? "button" : "button-deactivated"}>Jelovnik</button>
+		<button on:click={() => toggleButton("ponude")} id={!jelovnik ? "button" : "button-deactivated"}>Posebne ponude</button>
+	</div>
+	<img {src} alt="Slika jelovnika">
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		align-items: center;
+		justify-content: center;
+		zoom: 200%;
+	}
+
+	h1 {
+		color: #3E2A12;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	#address {
+		color:#3E2A12;
+		font-size: 1em;
+	}
+
+	#button {
+		background-color: Transparent;
+		background-repeat:no-repeat;
+		cursor:pointer;
+		overflow: hidden;
+		outline:none;
+		border-bottom-color: #D6923E;
+		color: #D6923E;
+		border-bottom-width: 2;
+		border-right: none;
+		border-top:none;
+		border-left: none;
+		margin: 3em;
+	}
+
+	#button-deactivated {
+		background-color: Transparent;
+		background-repeat:no-repeat;
+		cursor:pointer;
+		overflow: hidden;
+		outline:none;
+		border-bottom-color: #3E2A12;
+		border-bottom-width: 2;
+		border-right: none;
+		border-top:none;
+		border-left: none;
+		margin: 3em;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
